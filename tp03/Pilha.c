da6732b52c7b18c5cde4ef8c551b0ac168ba8fb4
João Simão <101663962+joaosiwao@users.noreply.github.com>
2022-10-24 21:11:12 -0300

    Não ta 100%

--- tp03/Pilha.c
+++ tp03/Pilha.c
@@ -13,14 +13,27 @@ struct pilha {
 
 /* Aloca espaço em memória e retorna uma pilha. */
 Pilha *pilha_cria() {
-  return NULL;
+  Pilha *pilha = (Pilha *)malloc(sizeof(Pilha));
+  pilha->pilha_alunos = (Aluno *)malloc(CAPACIDADE_INICIAL * sizeof(Aluno *));
+  pilha->capacidade_maxima = CAPACIDADE_INICIAL;
+  pilha->tamanho = 0;
+  return pilha;
 }
 
 /* Libera a memória de uma pilha previamente criada e atribui NULL ao ponteiro
  * pilha. Retorna 1 caso seja possível fazer a liberação e 0 caso a pilha
  * informada seja NULL. */
 int pilha_libera(Pilha **pilha) {
-  return -2;
+  if (pilha != NULL) {
+    if ((*pilha)->tamanho > 0) {
+      free((*pilha)->pilha_alunos);
+      (*pilha)->pilha_alunos = NULL;
+   }
+    free(*pilha);
+    *pilha = NULL;
+    return 1;
+  }
+  return 0;
 }
 
 /* Insere um aluno na Pilha. Retorna 1 se foi possível adicionar, 0 caso já
@@ -28,25 +41,75 @@ int pilha_libera(Pilha **pilha) {
  * adicionado) e -1 caso a pilha ou aluno sejam NULL.
  */
 int pilha_push(Pilha *pilha, Aluno *aluno) {
-  return -2;
+  if (pilha == NULL || aluno == NULL) {
+    return -1;
+  }
+
+  int matricula;
+  char nome[50];
+  char curso[30];
+  alu_acessa(aluno, &matricula, nome, curso);
+  Aluno *aluno_aux = pilha_busca(pilha, matricula);
+  if (aluno_aux != NULL) {
+    return 0;
+  }
+  if (pilha->tamanho == pilha->capacidade_maxima) {
+    int nova_capacidade = 2 * pilha->capacidade_maxima;
+    pilha->pilha_alunos = (Aluno *)realloc(pilha->pilha_alunos, nova_capacidade);
+    pilha->capacidade_maxima = nova_capacidade;
+  }
+
+  (&(pilha->pilha_alunos))[pilha->tamanho++] = aluno;
+
+  return 1;
 }
+
 
 /* Remove um aluno na pilha. Retorna o aluno ou NULL caso a pilha esteja vazia
  * ou seja NULL. */
 Aluno *pilha_pop(Pilha *pilha) {
-  return NULL;
+  if (pilha == NULL || pilha->tamanho == 0) {
+    return NULL;
+  }
+// consertar
+  Aluno *aluno = (&(pilha->pilha_alunos))[pilha->tamanho];
+
+  for (int i = 1; i < pilha->tamanho; i++) {
+    (&(pilha->pilha_alunos))[i - 1] = (&(pilha->pilha_alunos))[i];
+  }
+  pilha->tamanho--;
+  (&(pilha->pilha_alunos))[pilha->tamanho] = NULL;
+
+  return aluno;
 }
 
 /* Recupera o primeiro aluno da pilha. Retorna o aluno ou NULL caso a pilha
  * esteja vazia ou seja NULL. */
 Aluno *pilha_top(Pilha *pilha) {
-  return NULL;
+    if (pilha == NULL || pilha->tamanho == 0) {
+    return NULL;
+  }
+  return (&(pilha->pilha_alunos))[pilha->tamanho];
 }
 
 /* Busca aluno pelo número de matricula. Retorna o aluno se este estiver na
  * lista e NULL caso contrário, isto é, (i) pilha vazia; (ii) não exista aluno
  * com a matricula fornecida; ou (iii) a pilha seja NULL */
 Aluno *pilha_busca(Pilha *pilha, int matricula) {
+  if (pilha != NULL && pilha->tamanho > 0) {
+
+    int matricula_aux;
+    char nome[50];
+    char curso[30];
+
+    for (int i = 0; i < pilha->tamanho; i++) {
+      alu_acessa((&(pilha->pilha_alunos))[i], &matricula_aux, nome, curso);
+      if (matricula == matricula_aux) {
+        return (&(pilha->pilha_alunos))[i];
+      }
+    }
+  }
+
   return NULL;
 }
 
@@ -54,12 +117,23 @@ Aluno *pilha_busca(Pilha *pilha, int matricula) {
  * não esteja vazia e -1 se p Pilha for NULL
  */
 int pilha_vazia(Pilha *pilha) {
-  return -2;
+  if (pilha == NULL) {
+    return -1;
+  }
+  if (pilha->tamanho == 0) {
+    return 1;
+  }
+  return 0;
 }
+
 
 /* Computa a quantidade de alunos na pilha. Retorna a quantidade de alunos
  * ou -1, caso a Pilha for NULL.
  */
 int pilha_quantidade(Pilha *pilha) {
-  return -2;
+  if (pilha == NULL) {
+    return -1;
+  }
+
+  return pilha->tamanho;
 }
